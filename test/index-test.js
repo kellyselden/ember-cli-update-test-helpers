710f87b175dab59928db2d4f7c102b37a798f11a
Kelly Selden <kellyselden@gmail.com>
2020-01-18 16:24:32 +0000

    increase timeout

--- test/index-test.js
+++ test/index-test.js
@@ -13,7 +13,7 @@ const fs = require('fs');
 const copyFile = promisify(fs.copyFile);
 
 describe(function() {
-  this.timeout(5 * 1000);
+  this.timeout(10 * 1000);
 
   describe(emberNew, function() {
     it('works', async function() {
