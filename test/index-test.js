223ca1cd42311875bc07befb3842519c62782690
Kelly Selden <602423+kellyselden@users.noreply.github.com>
2023-07-07 20:03:56 +0100

    use shorthand notation

--- test/index-test.js
+++ test/index-test.js
@@ -16,7 +16,7 @@ const copyFile = promisify(fs.copyFile);
 const cpr = promisify(require('cpr'));
 
 describe(function() {
-  this.timeout(15 * 1000);
+  this.timeout(15e3);
 
   describe(emberNew, function() {
     it('works', async function() {
